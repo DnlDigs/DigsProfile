6d9208c49704776adb2d8d81f6d82ec3ecde2191
DnlDigs <dnldigol@gmail.com>
2025-04-24 13:25:12 -0400

    dsada

--- dnlprofile/src/main.jsx
+++ dnlprofile/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter basename="/DigsProfile">
+     <BrowserRouter basename="/DigsProfile/dnlprofile">
       <App />
     </BrowserRouter>
   </React.StrictMode>
